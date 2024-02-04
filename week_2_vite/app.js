5cc77435048fce0b32aaf7da302c1af1a6c68155
Zer02z2 <zongzechan@gmail.com>
2024-02-04 16:04:14 -0500

    Justify center content

--- week_2_vite/app.js
+++ week_2_vite/app.js
@@ -1,17 +1,17 @@
-let button = document.getElementById('duplicate-elephant');
-let countNum = document.getElementById('count');
-let rightColumn = document.getElementsByClassName('right')[0];
+// let button = document.getElementById('duplicate-elephant');
+// let countNum = document.getElementById('count');
+// let rightColumn = document.getElementsByClassName('right')[0];
 
-let count = 0;
+// let count = 0;
 
-button.addEventListener('click', () => {
-  let image = document.createElement('img');
-  image.setAttribute('src', './elephant.jpg');
-  rightColumn.appendChild(image);
-  console.log(rightColumn);
-  count += 1;
-  countNum.innerHTML = count;
-});
+// button.addEventListener('click', () => {
+//   let image = document.createElement('img');
+//   image.setAttribute('src', './elephant.jpg');
+//   rightColumn.appendChild(image);
+//   console.log(rightColumn);
+//   count += 1;
+//   countNum.innerHTML = count;
+// });
 
 
 
