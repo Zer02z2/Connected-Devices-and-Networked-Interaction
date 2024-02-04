0b1b6455e3af85c93c04376b9b22961cafdf9760
Zer02z2 <zongzechan@gmail.com>
2024-02-04 15:20:33 -0500

    Spinning animation
    images of elephants spin

--- week_2_vite/app.js
+++ week_2_vite/app.js
@@ -6,11 +6,12 @@ let count = 0;
 
 button.addEventListener('click', () => {
   let image = document.createElement('img');
-  image.setAttribute('src', 'https://hatrabbits.com/wp-content/uploads/2017/01/random-word-1.jpg');
+  image.setAttribute('src', './elephant.jpg');
   rightColumn.appendChild(image);
   console.log(rightColumn);
   count += 1;
   countNum.innerHTML = count;
 });
+
 
 
