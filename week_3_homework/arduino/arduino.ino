85d66b832667cd2051ac0b11796124597af1ec6a
Zer02z2 <zongzechan@gmail.com>
2024-02-14 17:29:47 -0500

    Revert "UPD"
    This reverts commit 198565f94b4e54bc6f06997c9c8980db08811c2e.

--- week_3_homework/arduino/arduino.ino
+++ week_3_homework/arduino/arduino.ino
@@ -1,6 +1,5 @@
 #include <WiFiNINA.h>
 #include <Arduino_LSM6DS3.h>
-#include "config.h"
 
 const int yellow = 2;
 const int blue = 3;
@@ -14,44 +13,46 @@ int lastGreen = 0;
 
 bool ready = true;
 
-WiFiUDP udpClient;
-
-//configure your WiFi information inside config.h file
+WiFiClient client;
 
 const char server[] = "10.23.10.119"; //local IP address of receiver device goes here
-const int port = 2222; //desired port # goes here. Make sure the receiver is listening on the same port!
+const int portNum = 2222; //desired port # goes here. Make sure the receiver is listening on the same port!
 
-
-//the port OTHER devices should use when sending to this one
-const int localPort = 5000;
+//be sure to remove WiFi network details before uploading this code!
+const char WIFI_SSID[] = "sandbox370"; //WiFi network name goes here
+const char WIFI_PASS[] = "+s0a+s03!2gether?"; //WiFi password goes here
 
 void setup() {
   Serial.begin(9600);
-  
-    if (!IMU.begin()) {
+  //retry connection until WiFi is connected successfully
+
+  //init IMU
+  if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
 
     while (1);
   }
 
-  //retry connection until WiFi is connected successfully
+  //init wifi
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Attempting to connect to SSID: ");
     // Attempt connection to WPA/WPA2 network.
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-    delay(500);
+    delay(3000);
   }
   Serial.println("connected!");
-
-  udpClient.begin(localPort);
 }
 
-char messageBuffer[256];
-
 void loop() {
-  if (millis()%100 < 10) {    
+  //connect to client if disconnected, or send TCP message if conected
+  if (!client.connected()) {
+    Serial.println("connecting");
+    client.connect(server, portNum);
+    delay(1000);
+    return;
+  } else {
     //add something more interesting here
-    udpClient.beginPacket(server, port);
+    Serial.println("sending TCP message");
 
     float x, y, z;
     int thisYellow = digitalRead(yellow);
@@ -86,19 +87,11 @@ void loop() {
 
     if (thisYellow == 0 && thisBlue == 0 && thisRed == 0 && thisGreen == 0) ready = true;
 
-    Serial.println("sending Udp");
-    udpClient.print(x);
-    udpClient.print(",");
-    udpClient.println(message);
-    delay(10);
+    client.print(x);
+    client.print(",");
+    client.println(message);
+    // client.println(data);
+    delay(100);
   }
-
-  //check for incoming packets
-  if (udpClient.parsePacket()) {
-    udpClient.read(messageBuffer, 255);
-    Serial.print("UDP received: ");
-    Serial.println(atoi(messageBuffer));
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  }
-  
 }
+
--- week_3_homework/arduino/config.h
+++
@@ -1,2 +0,0 @@
-const char WIFI_SSID[] = "sandbox370"; //WiFi network name goes here
-const char WIFI_PASS[] = "+s0a+s03!2gether?"; //WiFi password goes here
