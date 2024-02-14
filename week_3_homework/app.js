4c1ccab8e710924e4a8875349fab2d7014843e9e
Zer02z2 <zongzechan@gmail.com>
2024-02-14 17:50:09 -0500

    working

--- week_3_homework/app.js
+++ week_3_homework/app.js
@@ -1,6 +1,6 @@
 const socket = new WebSocket('ws://localhost:1880/testing');
 
-let textarea = document.querySelector('div.bottom textarea');
+let textarea = document.querySelector('div.bottom p2');
 let leftWords = document.querySelector('div.leftColumn .conversation');
 let rightWords = document.querySelector('div.rightColumn .conversation');
 let leftElephant = document.querySelector('div.leftColumn .elephant');
@@ -13,6 +13,7 @@ let rightLines = [document.querySelector('div.rightColumn .imageContainer .leftL
                 document.querySelector('div.rightColumn .imageContainer .rightLines')];
 
 let talkingSide = 'left';
+let textString = '';
 
 socket.addEventListener('open', handleSocketOpen);
 socket.addEventListener('message', handleSocketMessages);
@@ -22,7 +23,16 @@ function handleSocketOpen() {
 }
 function handleSocketMessages(e) {
     document.body.style.backgroundColor = 'white';
-    document.querySelector('textarea').innerHTML = e.data;
+    let data = e.data.split(',');
+    
+    if (data[0] > 0) switchToLeft();
+    else if (data[0] < 0) switchToRight();
+
+    if (data[1] == 'yellow') textString += '~';
+    else if (data[1] == 'blue') textString += '!';
+    else if (data[1] == 'red') textString += '?';
+    else if (data[1] == 'green') sendMessage();
+    textarea.innerHTML = textString;
 }
 
 for (const i of leftLines) {
@@ -38,25 +48,18 @@ const addPhrase = (speaker, listener) => {
     }
 
     let phrase = document.createElement('p');
-    phrase.innerHTML = textarea.value + '<br><br>';
+    phrase.innerHTML = textString + '<br><br>';
     speaker.appendChild(phrase);
 
     let spacing = document.createElement('p');
     spacing.innerHTML = '<br><br>';
     listener.appendChild(spacing);
 
-    textarea.value = null;
+    textString = '';
 }
 
-const resetAnimation = (el) => {
-
-    el.style.animation = 'none';
-    el.offsetHeight;
-    el.style.animation = null;
-}
-
-leftElephant.addEventListener('click', () => {
-
+const switchToLeft = () => {
+    
     talkingSide = 'left';
     span.innerHTML = 'Left Elephant says:';
     for (const i of leftLines) {
@@ -65,8 +68,9 @@ leftElephant.addEventListener('click', () => {
     for (const i of rightLines) {
         i.style.visibility = 'hidden';
     }
-});
-rightElephant.addEventListener('click', () => {
+}
+
+const switchToRight = () => {
 
     talkingSide = 'right';
     span.innerHTML = 'Right Elephant says:';
@@ -76,25 +80,40 @@ rightElephant.addEventListener('click', () => {
     for (const i of rightLines) {
         i.style.visibility = 'visible';
     }
-});
+}
+
+const resetAnimation = (el) => {
+
+    el.style.animation = 'none';
+    el.offsetHeight;
+    el.style.animation = null;
+}
+
+leftElephant.addEventListener('click', switchToLeft);
+rightElephant.addEventListener('click', switchToRight);
 
 // Enter key to send input
 textarea.addEventListener('keyup', (e) => {
 
     if (e.key === 'Enter') {
 
-        if (talkingSide === 'left') {
-            addPhrase(leftWords, rightWords);
-            resetAnimation(leftElephant);
-            leftElephant.style.animation = 'rotate 0.5s linear';
-        }
-        else {
-            addPhrase(rightWords, leftWords);
-            resetAnimation(rightElephant);
-            rightElephant.style.animation = 'rotate 0.5s reverse linear';
-        }
+        sendMessage();
     }
 })
+
+function sendMessage() {
+
+    if (talkingSide === 'left') {
+        addPhrase(leftWords, rightWords);
+        resetAnimation(leftElephant);
+        leftElephant.style.animation = 'rotate 0.5s linear';
+    }
+    else {
+        addPhrase(rightWords, leftWords);
+        resetAnimation(rightElephant);
+        rightElephant.style.animation = 'rotate 0.5s reverse linear';
+    }
+}
 
 
 
